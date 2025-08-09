2763a03179f5210e63114f934b19e7d4997210b1
FakeCafeBazar <hossein.naseran@gmail.com>
2025-08-10 01:03:24 +0330

    v1

---
+++ script.js
@@ -0,0 +1,123 @@
+let container = document.querySelector(".container")
+let search = document.querySelector("input");
+let result =  document.querySelector(".result");
+let body = document.querySelector("body")
+let favorite = document.querySelector(".favorite")
+let button = document.querySelector("button").addEventListener("click" ,() => {
+    
+    if (search.value === "") {
+        alert("چیزی وارد کن");
+    }
+    else if (!search.value === Number) {
+    alert("عدد وارد کن");
+    }
+    
+    
+    fetch(`https://jsonplaceholder.typicode.com/posts?id=${search.value}`)
+    .then(response => response.json())
+    .then(data => {
+        
+        // let post = document.createElement("div");
+        
+        //     post.className = "post";
+        //   post.textContent = data
+        
+        // let post = result.createElement("div");
+        // post.className ="post";
+        // post.textContent = data 
+        //result = document.createElement("p")
+        
+        
+        // let post = ""
+        // for (let index in data) {
+            //      post += data[index] + "";
+            
+            // }
+            // result.innerHTML = post
+            
+            // console.log(post)
+            
+                  //                      Inner Html              //
+            
+            let  value =  Object.values(data[0]);
+            let  keys = Object.keys(data[0])
+            result.innerHTML =  "<h4>" + keys[0] + " : " + value[0] +  "</h4>"  +
+            "<h4>" + keys[1] + " : " + value[1] + "</h4>" +
+            "<h4>" + keys[2] + " : " + value[2] + "</h4>" +
+            "<h4>" + keys[3] + " : " + value[3] + "</h4>" +
+            "<a>"  +"افزودن به علاقمندی ها" +   "</a>"
+
+
+
+
+
+               //                  Create Element                    //  
+
+            
+
+
+
+
+
+
+
+
+
+
+
+               //                log                //
+            
+            console.log(keys[0] +":" + value[0] , '\n' ,
+                keys[1] + ":" + value[1],'\n' ,
+                keys[2] + ":" + value[2],'\n' ,
+                keys[3] + ":" + value[3],'\n' 
+            );
+
+
+
+            
+
+
+
+
+       let a = document.querySelector("a");
+        a.onclick =()=> {
+            alert("افزوده شد");
+            let favoritePost = document.createElement("div");
+            favoritePost.textContent = keys[1]+" : "+value[1];
+           favorite.prepend(favoritePost)
+           favoritePost.className = "favorite-post"
+         
+        }
+
+       
+
+
+
+
+  
+   
+
+        
+
+    })
+    .catch(error => console.error(error))
+    
+ })
+ 
+ // button.onclick = function () {   
+//     fetch(`https://jsonplaceholder.typicode.com/posts?id=${search.value}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         let post = document.createElement("div")
+//         post.textContent = data;
+    
+    
+    
+//     })
+//      .catch(error => console.error(error))
+// }
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(response => response.json())
+// .then(data => console.log(data))
