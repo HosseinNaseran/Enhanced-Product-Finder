d660fad22bbce786e79144fd14560cc4cf516ccf
HosseinNaseran <hossein.naseran@gmail.com>
2025-08-23 23:55:13 +0330

    split eror

--- script.js
+++ script.js
@@ -3,42 +3,46 @@ let search = document.querySelector("input");
 let result =  document.querySelector(".result");
 let body = document.querySelector("body")
 let favorite = document.querySelector(".favorite")
+
+
+// search.addEventListener("change",function(){
+//     function text() {
+//      return search.value.split(",")
+//     }
+//     console.log(text())
+
+// //  result.innerHTML = text();
+// })
+
+
+
+
+
 let button = document.querySelector("button").addEventListener("click" ,() => {
     
     if (search.value === "") {
         alert("چیزی وارد کن");
     }
-    else if (!search.value === Number) {
-    alert("عدد وارد کن");
-    }
+  
+   
     
     
     fetch(`https://jsonplaceholder.typicode.com/posts?id=${search.value}`)
     .then(response => response.json())
     .then(data => {
         
-        // let post = document.createElement("div");
-        
-        //     post.className = "post";
-        //   post.textContent = data
-        
-        // let post = result.createElement("div");
-        // post.className ="post";
-        // post.textContent = data 
-        //result = document.createElement("p")
-        
-        
-        // let post = ""
-        // for (let index in data) {
-            //      post += data[index] + "";
-            
-            // }
-            // result.innerHTML = post
-            
-            // console.log(post)
-            
+      
+             function text() {
+     return search.value.split(",")
+    }
+    console.log(text())
             let  value =  Object.values(data[0]);
             let  keys = Object.keys(data[0])
+
+
+
+
+            
                   //                      Inner Html              //
             
             // result.innerHTML =  "<h4>" + keys[0] + " : " + value[0] +  "</h4>"  +
@@ -53,30 +57,46 @@ let button = document.querySelector("button").addEventListener("click" ,() => {
 
                //                  Create Element                    //  
 
-            let post = document.createElement("div");
-            post.className = "post";
-            post.textContent = keys[0] + " : " + value[0] + '\n' +
-            keys[1] + " : " + value[1] +  '\n' +
-            keys[2] + " : " + value[2] +  '\n' +
-            keys[3] + " : " + value[3] ;
+            // let post = document.createElement("div");
+            // post.className = "post";
+            // post.textContent = keys[0] + " : " + value[0] + '\n' +
+            // keys[1] + " : " + value[1] +  '\n' +
+            // keys[2] + " : " + value[2] +  '\n' +
+            // keys[3] + " : " + value[3] ;
 
 
-            result.prepend(post);
+            // result.prepend(post);
             
 
 
-
+   
 
 
 
 
                //                log                //
+
+
+            // console.log(keys[0] +":" + value[0] , '\n' ,
+            //     keys[1] + ":" + value[1],'\n' ,
+            //     keys[2] + ":" + value[2],'\n' ,
+            //     keys[3] + ":" + value[3],'\n' 
+            // );
+
+
+
+        //   let text = []
+        //     text.push(data)
+        //     console.log(text);
             
-            console.log(keys[0] +":" + value[0] , '\n' ,
-                keys[1] + ":" + value[1],'\n' ,
-                keys[2] + ":" + value[2],'\n' ,
-                keys[3] + ":" + value[3],'\n' 
-            );
+
+               
+
+
+
+
+
+
 
 
 
@@ -85,32 +105,38 @@ let button = document.querySelector("button").addEventListener("click" ,() => {
 
 
  
-    //    let a = document.querySelector("a");
-    let a = document.createElement("a");
-     result.prepend(a)
-     a.textContent = "افزودن به علاقه مندی ها"
-        a.onclick =()=> {
-            alert("افزوده شد");
-            let favoritePost = document.createElement("div");
-            favoritePost.textContent = keys[1]+" : "+value[1];
-           favorite.prepend(favoritePost)
-           favoritePost.className = "favorite-post"
+ 
+    // let a = document.createElement("a");
+    //  result.prepend(a)
+    //  a.textContent = "افزودن به علاقه مندی ها"
+    //     a.onclick =()=> {
+    //         alert("افزوده شد");
+    //         let favoritePost = document.createElement("div");
+    //         favoritePost.textContent = keys[1]+" : "+value[1];
+    //        favorite.prepend(favoritePost)
+    //        favoritePost.className = "favorite-post"
            
-           let close = document.createElement("span")
-           close.className = "close"
-           close.textContent = "\u2715"
-           favoritePost.prepend(close)
-           close.onclick=()=>{
-            favoritePost.remove();
-           }
-        }
+    //        let close = document.createElement("span")
+    //        close.className = "close"
+    //        close.textContent = "\u2715"
+    //        favoritePost.prepend(close)
+    //        close.onclick=()=>{
+    //         favoritePost.remove();
+    //        }
+    //     }
 
 
-       
+  
+    // function text() {
 
+    //     return search.value.split(",");
+    // }
+    // result.innerHTML = text().join("<br>");
 
+  
+   
 
-
+    
   
    
 
@@ -120,20 +146,3 @@ let button = document.querySelector("button").addEventListener("click" ,() => {
     .catch(error => console.error(error))
     
  })
- 
- // button.onclick = function () {   
-//     fetch(`https://jsonplaceholder.typicode.com/posts?id=${search.value}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         let post = document.createElement("div")
-//         post.textContent = data;
-    
-    
-    
-//     })
-//      .catch(error => console.error(error))
-// }
-
-// fetch("https://jsonplaceholder.typicode.com/posts")
-// .then(response => response.json())
-// .then(data => console.log(data))
