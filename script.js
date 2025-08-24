d557fa27fbfe812938bac7f853b401505ff76bed
HosseinNaseran <hossein.naseran@gmail.com>
2025-08-25 00:16:18 +0330

    Product authentication

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ let favorite = document.querySelector(".favorite")
 
 
 // search.addEventListener("change",function(){
-//     function text() {
-//      return search.value.split(",")
-//     }
-//     console.log(text())
-
-// //  result.innerHTML = text();
-// })
-
-
-
-
-
-let button = document.querySelector("button").addEventListener("click" ,() => {
-    
-    if (search.value === "") {
+    //     function text() {
+        //      return search.value.split(",")
+        //     }
+        //     console.log(text())
+        
+        // //  result.innerHTML = text();
+        // })
+        
+        
+        
+        
+        
+        let button = document.querySelector("button").addEventListener("click" ,() => {
+            
+            if (search.value === "") {
         alert("چیزی وارد کن");
     }
-  
+    
    
     
     
@@ -31,99 +31,126 @@ let button = document.querySelector("button").addEventListener("click" ,() => {
     .then(response => response.json())
     .then(data => {
         
-      
-             function text() {
-     return search.value.split(",")
-    }
-    console.log(text())
+        
+        //          function text() {
+            //  return search.value.split(",")
+            // }
+            // console.log(text())
+            
+            
+            
             let  value =  Object.values(data[0]);
             let  keys = Object.keys(data[0])
-
-
-
-
             
-                  //                      Inner Html              //
+            
+            
+            
+            
+            //                      Inner Html              //
             
             // result.innerHTML =  "<h4>" + keys[0] + " : " + value[0] +  "</h4>"  +
             // "<h4>" + keys[1] + " : " + value[1] + "</h4>" +
             // "<h4>" + keys[2] + " : " + value[2] + "</h4>" +
             // "<h4>" + keys[3] + " : " + value[3] + "</h4>" +
             // "<a>"  +"افزودن به علاقمندی ها" +   "</a>"
+            
+            
+            
+            
+            
+            //                  Create Element                    //  
+            
+            let post = document.createElement("div");
+            post.className = "post";
+            post.textContent = keys[0] + " : " + value[0] + '\n' +
+            keys[1] + " : " + value[1] +  '\n' +
+            keys[2] + " : " + value[2] +  '\n' +
+            keys[3] + " : " + value[3] ;
+            
+            
+            result.prepend(post);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //                log                //
 
-
-
-
-
-               //                  Create Element                    //  
-
-            // let post = document.createElement("div");
-            // post.className = "post";
-            // post.textContent = keys[0] + " : " + value[0] + '\n' +
-            // keys[1] + " : " + value[1] +  '\n' +
-            // keys[2] + " : " + value[2] +  '\n' +
-            // keys[3] + " : " + value[3] ;
-
-
-            // result.prepend(post);
+            
+            console.log(keys[0] +":" + value[0] , '\n' ,
+                keys[1] + ":" + value[1],'\n' ,
+                keys[2] + ":" + value[2],'\n' ,
+                keys[3] + ":" + value[3],'\n' 
+            );
+            
+            
+            
+            //   let text = []
+            //     text.push(data)
+            //     console.log(text);
             
 
-
-   
-
-
-
-
-               //                log                //
-
-
-            // console.log(keys[0] +":" + value[0] , '\n' ,
-            //     keys[1] + ":" + value[1],'\n' ,
-            //     keys[2] + ":" + value[2],'\n' ,
-            //     keys[3] + ":" + value[3],'\n' 
-            // );
-
-
-
-        //   let text = []
-        //     text.push(data)
-        //     console.log(text);
+            
+            
+            
+            
+            
+            
+            
             
 
+            
+            
+            
+            
+            
+            
+ 
+            let a = document.createElement("a");
+            result.prepend(a)
+            a.textContent = "افزودن به علاقه مندی ها"
+            a.onclick =()=> {
+                
+                
+                
+                
+                
                
+                    if (favorite.textContent.includes(value[1])) {
+                        alert("این محصول از قبل وجود دارد");
+                    
+                    }
+                    else{
+
+                        alert("افزوده شد");
+                         let favoritePost = document.createElement("div");
+        
+                          favoritePost.textContent = keys[1]+" : "+value[1];
+                         favorite.prepend(favoritePost)
+                         favoritePost.className = "favorite-post"
+                         
+                         let close = document.createElement("span")
+                         close.className = "close"
+                         close.textContent = "\u2715"
+                         favoritePost.prepend(close)
+                         close.onclick=()=>{
+                          favoritePost.remove();
+                         }
+                    }
+                
+                
+                }
 
 
 
 
 
-
-
-
-
-            
-
-
-
- 
- 
-    // let a = document.createElement("a");
-    //  result.prepend(a)
-    //  a.textContent = "افزودن به علاقه مندی ها"
-    //     a.onclick =()=> {
-    //         alert("افزوده شد");
-    //         let favoritePost = document.createElement("div");
-    //         favoritePost.textContent = keys[1]+" : "+value[1];
-    //        favorite.prepend(favoritePost)
-    //        favoritePost.className = "favorite-post"
-           
-    //        let close = document.createElement("span")
-    //        close.className = "close"
-    //        close.textContent = "\u2715"
-    //        favoritePost.prepend(close)
-    //        close.onclick=()=>{
-    //         favoritePost.remove();
-    //        }
-    //     }
+        
 
 
   
