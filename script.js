986b4560b831500a51bff1bc9f0c532ddfe1d4c4
HosseinNaseran <hossein.naseran@gmail.com>
2025-08-29 23:27:59 +0330

    split error fixed

--- script.js
+++ script.js
@@ -1,102 +1,71 @@
-let container = document.querySelector(".container")
-let search = document.querySelector("input");
-let result = document.querySelector(".result");
-let body = document.querySelector("body")
-let favorite = document.querySelector(".favorite")
+// const container = document.querySelector(".container");
+const search = document.querySelector("input");
+const result = document.querySelector(".result");
+// const body = document.querySelector("body");
+const favorite = document.querySelector(".favorite");
 
+document.querySelector("button").addEventListener("click", () => {
+  if (search.value === "") {
+    alert("چیزی وارد کن");
+    return; 
+  }
 
+  const parsed = search.value.split(",").map((value, _) => {
+    return value.trim();
+  });
 
-let button = document.querySelector("button").addEventListener("click", () => {
+  for (const item of parsed) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?id=${item}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const value = Object.values(data[0]);
+        const keys = Object.keys(data[0]);
 
-function text() {
-    return search.value.split(",")
-        
-}
-console.log(text())
+        const post = document.createElement("div");
+        post.className = "post";
+        post.textContent =
+          keys[0] +
+          " : " +
+          value[0] +
+          "\n" +
+          keys[1] +
+          " : " +
+          value[1] +
+          "\n" +
+          keys[2] +
+          " : " +
+          value[2] +
+          "\n" +
+          keys[3] +
+          " : " +
+          value[3];
 
+        result.prepend(post);
 
-let texts = []
-texts.push(text())
-console.log(typeof(texts) , (texts[0]))
+        const a = document.createElement("a");
+        result.prepend(a);
+        a.textContent = "افزودن به علاقه مندی ها";
+        a.onclick = () => {
+          if (favorite.textContent.includes(value[1])) {
+            alert("این محصول از قبل وجود دارد");
+          } else {
+            alert("افزوده شد");
+            const favoritePost = document.createElement("div");
 
+            favoritePost.textContent = keys[1] + " : " + value[1];
+            favorite.prepend(favoritePost);
+            favoritePost.className = "favorite-post";
 
-    if (search.value === "") {
-        alert("چیزی وارد کن");
-    }
-    fetch(`https://jsonplaceholder.typicode.com/posts?id=${texts}`)
-        // fetch(`https://jsonplaceholder.typicode.com/posts?id=${search.value}`)
-        .then(response => response.json())
-        .then(data => {
-
-
-
-            let value = Object.values(data[0]);
-            let keys = Object.keys(data[0])
-
-            //                      Inner Html              //
-
-            // result.innerHTML =  "<h4>" + keys[0] + " : " + value[0] +  "</h4>"  +
-            // "<h4>" + keys[1] + " : " + value[1] + "</h4>" +
-            // "<h4>" + keys[2] + " : " + value[2] + "</h4>" +
-            // "<h4>" + keys[3] + " : " + value[3] + "</h4>" +
-            // "<a>"  +"افزودن به علاقمندی ها" +   "</a>"
-
-
-            //                  Create Element                    //  
-
-            let post = document.createElement("div");
-            post.className = "post";
-            post.textContent = keys[0] + " : " + value[0] + '\n' +
-                keys[1] + " : " + value[1] + '\n' +
-                keys[2] + " : " + value[2] + '\n' +
-                keys[3] + " : " + value[3];
-
-
-            result.prepend(post);
-
-
-            //                log                //
-
-
-            console.log(keys[0] + ":" + value[0], '\n',
-                keys[1] + ":" + value[1], '\n',
-                keys[2] + ":" + value[2], '\n',
-                keys[3] + ":" + value[3], '\n'
-            );
-
-
-
-
-
-            let a = document.createElement("a");
-            result.prepend(a)
-            a.textContent = "افزودن به علاقه مندی ها"
-            a.onclick = () => {
-
-
-                if (favorite.textContent.includes(value[1])) {
-                    alert("این محصول از قبل وجود دارد");
-
-                }
-                else {
-
-                    alert("افزوده شد");
-                    let favoritePost = document.createElement("div");
-                    favoritePost.textContent = keys[1] + " : " + value[1];
-                    favorite.prepend(favoritePost)
-                    favoritePost.className = "favorite-post"
-
-                    let close = document.createElement("span")
-                    close.className = "close"
-                    close.textContent = "\u2715"
-                    favoritePost.prepend(close)
-                    close.onclick = () => {
-                        favoritePost.remove();
-                    }
-                }
-            }
-
-        })
-        .catch(error => console.error(error))
-
-})
+            const close = document.createElement("span");
+            close.className = "close";
+            close.textContent = "\u2715";
+            favoritePost.prepend(close);
+            close.onclick = () => {
+              favoritePost.remove();
+            };
+          }
+        };
+      })
+      .catch((error) => console.error(error));
+  }
+});
